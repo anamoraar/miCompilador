fc4a05eb899f274474679d0dff8a2ccb08cede0a
Ana <anamora02@estudiantec.cr>
2023-11-03 18:06:04 -0600

    Leer input por archivo

---
+++ README.md
@@ -0,0 +1,16 @@
+# Proyecto 2 - miCompilador
+
+## Grupo 4:
+- Ana Laura Mora Arias - 2021066978
+- Derinia Zhang Gu - 2021077766 
+- Josué Castro Ramírez - 2020065036 
+
+## Probar la fase léxica
+Para ejecutar el siguiente comando se debe estar en el root del proyecto, es decir, en miCompilador:
+```bash
+java -cp "lib/antlr-4.13.1-complete.jar;out/production/miCompilador" PruebaLexer IO/prueba1.txt IO/output1.txt
+```
+
+## Requisitos
+
+1. Versión de Java >= 16.
--- src/PruebaLexer.java
+++ src/PruebaLexer.java
@@ -1,18 +1,33 @@
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class PruebaLexer {
-    public static void main(String[] args) throws Exception {
-        String inputText = "x := 42;\n var := falso;";
-
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.err.println("Use el comando así: PruebaLexer nombreArchivoEntrada nombreArchivoSalida");
+            System.exit(1);
+        }
+        //Nombre de los archivos de entrada y salida
+        String inputFileName = args[0];
+        String outputFileName = args[1];
+        //Pasarlo a string
+        FileInputStream inputStream = new FileInputStream(inputFileName);
+        String inputText = new String(inputStream.readAllBytes());
+        //Generar el lexer y los tokens dada la entrada
         GramaticaLexer lexer = new GramaticaLexer(CharStreams.fromString(inputText));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-
         tokens.fill();
-
+        //Archivo de salida
+        //Escribir en el archivo de salia
+        FileWriter outputWriter = new FileWriter(outputFileName);
         for (Token token : tokens.getTokens()) {
-            System.out.println("Token: " + token.getText() + ", línea: " + token.getLine() + ", índice: " + token.getCharPositionInLine());
+            outputWriter.write("Token: " + token.getText() + ", línea: " + token.getLine() + ", índice: " + token.getCharPositionInLine() + "\n");
         }
+        outputWriter.close();
     }
 }
+
